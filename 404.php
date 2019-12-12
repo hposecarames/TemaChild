8b93f11405622e81a4adc8ed5713cd364aa7be34
EC2 Default User <ec2-user@ip-172-31-22-107.eu-west-1.compute.internal>
2019-12-12 21:09:00 +0000

    Tema child

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+	<div class="page-404">
+	
+		<h2><?php esc_html_e( 'Va a ser que no', 'ashe' ); ?></h2>
+
+		<p>
+			<?php esc_html_e( 'Lo que buscas ya no está aquí', 'ashe' ); ?>
+		</p>
+
+	</div>
+
+<?php get_footer("404"); ?>
